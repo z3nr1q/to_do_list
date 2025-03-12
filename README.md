dc7dcc4de210c95c9988389249ab0f1c4fc89f48
José Henrique <github@zenriq.com>
2025-03-12 15:13:53 -0300

    Inicialização do projeto: Implementação básica da Lista de Tarefas

---
+++ README.md
@@ -0,0 +1,23 @@
+# Lista de Tarefas
+
+Um aplicativo Flutter para gerenciamento de tarefas diárias, desenvolvido para aprendizado de desenvolvimento Android.
+
+## Funcionalidades Atuais
+
+- Adicionar novas tarefas
+- Remover tarefas existentes
+- Interface intuitiva e responsiva
+
+## Próximas Funcionalidades
+
+- Persistência de dados
+- Marcação de tarefas concluídas
+- Categorização de tarefas
+- Datas de vencimento
+
+## Como Executar
+
+1. Certifique-se de ter o Flutter instalado
+2. Clone este repositório
+3. Execute `flutter pub get` para instalar as dependências
+4. Execute `flutter run` para iniciar o aplicativo
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista de Tarefas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final List<String> _todos = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addTodo(String todo) {
+    if (todo.isNotEmpty) {
+      setState(() {
+        _todos.add(todo);
+      });
+      _controller.clear();
+    }
+  }
+
+  void _removeTodo(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minhas Tarefas'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: 'Digite uma nova tarefa',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () => _addTodo(_controller.text),
+                  child: const Text('Adicionar'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_todos[index]),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => _removeTodo(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
